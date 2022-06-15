28d45e217597d009ef420040955db0a5b2451ecb
Alejandro Gonzalez Sole <guatedude@gmail.com>
2022-06-15 11:21:53 -0400

    chore: adds simple test to buildFilters

--- src/build-filters.spec.ts
+++ src/build-filters.spec.ts
@@ -1,6 +1,12 @@
 import { buildFilter } from './build-filter';
 
 describe('buildFilter', () => {
+  it('creates a filter RegExp given a filter string', () => {
+    const result = buildFilter('.');
+
+    expect(result).toEqual(/^\/?\.$/i);
+  });
+
   it('creates a filter RegExp given a wildcard filter string', () => {
     const result = buildFilter('*');
 
